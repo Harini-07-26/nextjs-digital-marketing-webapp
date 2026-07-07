35b740bf216aadb7bb1733725d1a4986001af49c
Harini T <harini.t@nibavlifts.com>
2026-07-07 16:00:29 +0530

    feat: integrated contact form and getting saved in static config json file

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -2,7 +2,7 @@ import Resources from '@/src/components/resources';
 
 const ResourcesPage = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-background mx-10">
       <Resources />
     </div>
   );
